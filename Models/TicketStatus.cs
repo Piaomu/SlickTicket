cb04c1a365fcaddb9e2a9707f8d273722b29b44b
piaomu <driftwood.wahl@gmail.com>
2021-05-25 09:48:50 -0500

    Added TicketPriority, TicketStatus, TicketType models.

---
+++ Models/TicketStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SlickTicket.Models
+{
+    public class TicketStatus
+    {
+        public int Id { get; set; }
+        [DisplayName("Ticket Status")]
+        public string Name { get; set; }
+    }
+}
---
+++ Models/TicketType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SlickTicket.Models
+{
+    public class TicketType
+    {
+        // Primary Key
+        public int Id { get; set; }
+
+        [DisplayName("Ticket Type")]
+        public string Name { get; set; }
+    }
+}
