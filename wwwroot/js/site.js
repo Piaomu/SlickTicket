0e18294823a1235d60c906ec0b87cae45c5451fa
piaomu <driftwood.wahl@gmail.com>
2021-07-09 18:33:58 -0500

    Archive Display Toggle

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -12,3 +12,31 @@ function confirmDelete(uniqueId, isDeleteClicked) {
     }
 }
 
+function ticketToggle() {
+    let openToggle = document.getElementById('option_a1');
+    let archiveToggle = document.getElementById('option_a2');
+    let allToggle = document.getElementById('option_a3');
+
+    let openTickets = document.getElementById('openTickets');
+    let archivedTickets = document.getElementById('archivedTickets');
+    let allTickets = document.getElementById('allTickets');
+
+    if (openToggle.classList.contains('active')) {
+        $(openTickets).show();
+        $(archivedTickets).hide();
+        $(allTickets).hide();
+    }
+
+    if (archiveToggle.classList.contains('active')) {
+        $(openTickets).hide();
+        $(archivedTickets).show();
+        $(allTickets).hide();
+    }
+
+    if (allToggle.classList.contains('active')) {
+        $(openTickets).hide();
+        $(archivedTickets).hide();
+        $(allTickets).show();
+    }
+}
+
